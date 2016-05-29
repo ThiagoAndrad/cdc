2ea5143731a162dc18e386fc83236acf605ef518
ThiagoAndrad <thpandrade@gmail.com>
2016-05-28 23:09:48 -0300

    [Novos campos] criando novos atributos para produto

--- src/main/java/br/com/cdc/loja/models/Product.java
+++ src/main/java/br/com/cdc/loja/models/Product.java
@@ -1,5 +1,9 @@
 package br.com.cdc.loja.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +20,8 @@ public class Product {
 	@Lob
 	private String description;
 	private int pages;
+	@ElementCollection
+	private List<Price> prices = new ArrayList<Price>();
 	
 	public String getTitle() {
 		return title;
---
+++ src/main/java/br/com/cdc/loja/models/enuns/BookType.java
@@ -0,0 +1,7 @@
+package br.com.cdc.loja.models.enuns;
+
+public enum BookType {
+	
+	EBOOK, PRINTED, COMBO
+
+}
