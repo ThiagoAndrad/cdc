49cb94968446fe072eba16bb609a3ffb489e2d77
ThiagoAndrad <thpandrade@gmail.com>
2016-07-24 01:23:39 -0300

    [Validator] adicionando validacoes ao adicionar novo produto

--- src/main/java/br/com/cdc/loja/controllers/ProductsController.java
+++ src/main/java/br/com/cdc/loja/controllers/ProductsController.java
@@ -5,7 +5,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.cdc.loja.daos.ProductDAO;
 import br.com.cdc.loja.models.Product;
 import br.com.cdc.loja.models.enuns.BookType;
+import br.com.cdc.loja.validation.ProductValidator;
 
 @Controller
 @Transactional
@@ -23,6 +27,11 @@ public class ProductsController {
 	@Autowired
 	private ProductDAO productDAO;
 	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.setValidator(new ProductValidator());
+	}
+	
 	@RequestMapping("/form")
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("products/form");
@@ -31,10 +40,13 @@ public class ProductsController {
 	}
 
 	@PostMapping
-	public String save(@Valid Product product, RedirectAttributes redirectAttributes) {
+	public ModelAndView save(@Valid Product product, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			return form();
+		}
 		productDAO.save(product);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso");
-		return "redirect:produtos";
+		return new ModelAndView("redirect:produtos");
 	}
 
 	@GetMapping
