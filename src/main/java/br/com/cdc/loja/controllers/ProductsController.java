a0d5bb2f806ff2b557a45260016c5c34bdc16492
ThiagoAndrad <thpandrade@gmail.com>
2016-07-24 00:42:13 -0300

    [Anotations] atualizando anotations para novas do Spring

--- src/main/java/br/com/cdc/loja/controllers/ProductsController.java
+++ src/main/java/br/com/cdc/loja/controllers/ProductsController.java
@@ -1,11 +1,13 @@
 package br.com.cdc.loja.controllers;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -28,14 +30,14 @@ public class ProductsController {
 		return modelAndView;
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
-	public String save(Product product, RedirectAttributes redirectAttributes) {
+	@PostMapping
+	public String save(@Valid Product product, RedirectAttributes redirectAttributes) {
 		productDAO.save(product);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso");
 		return "redirect:produtos";
 	}
 
-	@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
 	public ModelAndView list() {
 		ModelAndView modelAndView = new ModelAndView("products/list");
 		modelAndView.addObject("products", productDAO.list());
